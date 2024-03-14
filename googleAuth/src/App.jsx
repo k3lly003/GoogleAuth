14e2854452d0cb40fc56dcaa1f1781982454e86c
k3lly003 <ntirukelly@gmail.com>
2024-03-14 19:51:59 +0200

    added login and signup pages and neccessary routes

--- googleAuth/src/App.jsx
+++ googleAuth/src/App.jsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./page/Home";
+import Login from "./page/Login";
+import SignUp from "./page/SignUp";
 
 const App = () => {
   return (
     <>
-      <h1>App</h1>
+      <div>
+        <Router>
+          <header className="flex justify-between w-100 mx-5 my-5">
+            <h3 className="text-xl text-white">GoogleAuth</h3>
+            <span className="flex justify-end gap-5">
+              <Link to="/" className="text-xl text-white">
+                Home
+              </Link>
+              <Link
+                to="/login"
+                className="inline-block text-xl px-4 py-2 leading-none rounded bg-green-500 text-white border-red hover:border-transparent hover:text-red-500 hover:bg-blue mt-4 lg:mt-0"
+              >
+                Login
+              </Link>
+            </span>
+          </header>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Routes>
+        </Router>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
